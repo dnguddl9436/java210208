29f37c91c33cc6182c84e337ff2200366dbe7c07
dnguddl9436 <dnguddl9696@gmail.com>
2021-02-26 18:23:59 +0900

    210226

--- java210208/src/com/vo/EmpVO.java
+++ java210208/src/com/vo/EmpVO.java
@@ -2,6 +2,9 @@ package com.vo;
 
 public class EmpVO {
 	//insert here - 급여 명세서에 부서이름을 출력하기 위한 SELECT문을 가지고 결정해 보세요.
+	//여기 변수들은 모두 private로 한다. - 캡슐레이션(은닉화) - 보호(위변조), 직접 값을 변경 불가
+	//각각의 값을 유지하거나 재사용하는 문제는 인스턴스화에 따라 각각 다르게 활용 가능함.
+	//값을 초기화하는 방법에 대해서는... 두 가지가 있음. 1)setter, 2)생성자
 	private DeptVO	dVO			= null;
 	private int 	empno 		= 0;	//7566
 	private String 	ename 		= "";	//사원명
@@ -14,9 +17,23 @@ public class EmpVO {
 	private int 	deptno 		= 0;	//부서번호(FK)-외래키-조인시에 사용되는 컬럼-릴레이션이다.
 	//생성자의 경우 생략할 수 있고 생략되었을 때 JVM이 대신 제공해줌.
 	//그러나 한개라도 생성자 정의되어 있다면 제공안됨.(디폴트 생성자 포함-파라미터를 가지지 않는 생성자)
-	public EmpVO() {}
+	public EmpVO() {
+		System.out.println("EmpVO 디폴트 생성자");
+	}
 	public EmpVO(int empno) {
 		this.empno = empno;//대입연산자- 그 값(지변)이 유지(다른클래스에서)됨
+	}
+	public EmpVO(int empno, String ename, String job, int mgr, 
+			String hiredate, double sal, double comm, int deptno, DeptVO dVO) {
+		this.empno = empno;//대입연산자- 그 값(지변)이 유지(다른클래스에서)됨
+		this.ename = ename;//전변과 초기화를 작성. 외부에서도 그 값을 유지 또는 활용 가능함.
+		this.job = job;
+		this.mgr = mgr;
+		this.hiredate = hiredate;
+		this.sal = sal;
+		this.comm = comm;
+		this.deptno = deptno;
+		this.dVO = dVO;
 	}
 	public int getEmpno() {
 		return empno;
