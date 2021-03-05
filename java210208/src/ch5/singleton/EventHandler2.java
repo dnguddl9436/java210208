03b71a838324f46c7457c4aa2557fd1093c38453
dnguddl9436 <dnguddl9696@gmail.com>
2021-03-05 18:37:51 +0900

    210305

---
+++ java210208/src/ch5/singleton/EventHandler2.java
@@ -0,0 +1,20 @@
+package ch5.singleton;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EventHandler2 implements ActionListener {
+	ViewApp2 viewApp = null;
+	//ViewApp viewApp = new ViewApp();
+	public EventHandler2(ViewApp2 viewApp) {
+		this.viewApp = viewApp;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==viewApp.jbtn) {//NullPointerException
+			System.out.println("전송버튼 호출 성공");
+		}
+	}
+
+}
